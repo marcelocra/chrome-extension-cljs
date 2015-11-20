c2f2c46db1db7db3bda64112c2c6bee1f5a68e50
Marcelo Almeida <marcelocralmeida@gmail.com>
2015-11-20 01:12:35 -0200

    Add more functions related to commands

--- src/chrome_extensions/background/events.cljs
+++ src/chrome_extensions/background/events.cljs
@@ -4,11 +4,29 @@
 
 (def constants {:commands {:tab-to-window "print-to-console"}})
 
-(defn command?
+;; COMMANDS.
+;;
+;; Check for commands. If the user press any of the available keyboard
+;; shortcuts, process accordingly.
+
+(defn- get-current-tab
+  [cb]
+  (.query js/chrome.tabs
+          #js {:active true :currentWindow true}
+          (fn [tabs]
+            (cb (first tabs)))))
+
+(defn- toggle-tab-to-window
+  []
+  (get-current-tab
+    (fn [tab]
+      (.log js/console (.-url tab)))))
+
+(defn- command?
   [command key]
   (= command (key (:commands constants))))
 
 (defn command-selector
   [command]
   (cond
-     (command? command :tab-to-window) (.log js/console command)))
+     (command? command :tab-to-window) (toggle-tab-to-window)))
